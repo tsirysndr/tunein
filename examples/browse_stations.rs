2a28a9961fdd10dfcc79767b08efa5f4f02eb186
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-02-22 21:40:05 +0300

    update examples

--- examples/browse_stations.rs
+++ examples/browse_stations.rs
@@ -3,7 +3,8 @@ use tunein::TuneInClient;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = TuneInClient::new();
-    let results = client.browse_by_id("s269487").await?;
+    let category_id = "c57943";
+    let results = client.browse_by_id(category_id).await?;
     println!("{}", serde_json::to_string_pretty(&results)?);
     Ok(())
 }
